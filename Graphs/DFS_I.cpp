5f1a058001a52a9e7e0a966919b6921deaed6f16
Manan <23.kapoormanan@gmail.com>
2025-11-27 20:50:06 +0530

    comments: full

--- Graphs/DFS_I.cpp
+++ Graphs/DFS_I.cpp
@@ -5,23 +5,26 @@
 using namespace std;
 
 void DFS(int start,vector<int> adj[],int n){
+    // we use a stack here
     stack<int> st;
+    // this is a visited array to keep a check of the nodes that we have visited
     vector<bool> visited(n,false);
 
+    //push the first node into the stack
     st.push(start);
 
     cout<<"DFS TRAVERSAL: ";
-    while(!st.empty()){
-        int node = st.top();
+    while(!st.empty()){ // iterate till the stack is not empty
+        int node = st.top(); // node is currently the first one
         st.pop();
-        if(!visited[node]){
+        if(!visited[node]){ // if that node is not visited then print it and visit it
             cout<<node<<" ";
             visited[node]=true;
         }
-        for(int i = adj[node].size()-1;i>=0;i--){
-            int neighbour = adj[node][i];
-            if(!visited[neighbour]){
-                st.push(neighbour);
+        for(int i = adj[node].size()-1;i>=0;i--){// loop from the adj list uve made for that specific node 
+            int neighbour = adj[node][i];// lastest neighbour to nearest one but all
+            if(!visited[neighbour]){ // if that neighbour is not visited
+                st.push(neighbour);// push it 
             }
         }
     }
