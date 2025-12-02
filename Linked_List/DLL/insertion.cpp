240e4c852f350dcd4ef9c2f3bf19870ce0162158
Manan <23.kapoormanan@gmail.com>
2025-12-03 02:37:29 +0530

    added code to DLL

--- Linked_List/DLL/insertion.cpp
+++ Linked_List/DLL/insertion.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Node{
+class Node {
     public:
     int data;
     Node* next;
     Node* back;
-    public:
-    Node(int val,Node* next1,Node* next2){
-        data=val;
-        next = next1;
-        back = next2;
+    Node(int x,Node*a ,Node* b){
+        this->data=x;
+        this->next=a;
+        this->back=b;
     }
-    public:
-    Node(int data1){
-        data=data1;
-        next = nullptr;
-        back = nullptr;
+    Node(int x){
+        this->data=x;
+        this->next=NULL;
+        this->back=NULL;
     }
 };
 
@@ -40,4 +39,47 @@ public:
             prev = temp;
         }
     }
+    void printList(){
+        Node* t = head;
+        do{
+            cout<<t->data<<" <-> ";
+            t=t->next;
+        }while (t!=NULL);
+        cout<<endl;
+
+    }
+    void insert(int x,int pos){
+        Node* t = new Node(x);
+        Node* p = head;
+        Node* mover = head;
+        if(pos==0){
+            //insert at before head 
+            t->next=p;
+            p->back=t;
+            t->back=NULL;
+            head=t;
+        }
+        else{
+            for(int i =0;i<pos-1;i++){
+                mover = mover->next;
+            }
+            // now our mover is on the node before we want to add
+            mover->next->back=t;
+            t->next=mover->next;
+            mover->next=t;
+            t->back=mover;
+        }
+
+    }
 };
+
+
+
+int main(){
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+    DLL d(arr);
+    d.printList();
+    d.insert(49,5);
+    d.printList();
+
+}
