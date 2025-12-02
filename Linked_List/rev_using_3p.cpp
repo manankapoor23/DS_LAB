3e49818c3a42be3d7040f19acba6b7b7753cc257
Manan <23.kapoormanan@gmail.com>
2025-12-03 00:35:46 +0530

    modifying links

---
+++ Linked_List/rev_using_3p.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+
+class Linked{
+    public:
+    Node* head;
+    Linked(){
+        head=NULL;
+    }
+    void insert(int value){
+        Node* newNode = new Node(value);
+        if(head==NULL){
+            head=newNode;
+            return;
+        }
+        Node* temp = head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+    }
+
+    void reverse(){
+        Node* p=head;
+        Node* q = NULL;
+        Node* r = NULL;
+        while(p!=NULL){
+            r=q;
+            q=p;
+            p=p->next;
+            q->next=r;
+        }
+        head = q;
+    }
+
+    void display(){
+        Node* temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+
+};
+
+int main(){
+    Linked list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.insert(4);
+    list.insert(5);
+    cout<<"Original List: ";
+    list.display();
+    list.reverse();
+    cout<<"Reversed List: ";
+    list.display();
+    return 0;
+
+}
