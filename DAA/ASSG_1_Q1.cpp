89dcbde425c4ec910a38ef2727beeb23bdffa6d2
Manan <23.kapoormanan@gmail.com>
2026-01-29 21:27:16 +0530

    assg 1

---
+++ DAA/ASSG_1_Q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int n = 10;
+    int target = 23;
+
+    cout << "Target found at index: "
+         << binarySearch(arr, n, target);
+}
